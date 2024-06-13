80e314bf46d11703001633d2f09f2eed3b5065eb
Matheus Cavalari <matheuscavbarbosa@hotmail.com>
2024-06-13 20:49:06 -0300

    Inclusão das configurações do banco de dados

--- RotasApi/Program.cs
+++ RotasApi/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
+
+builder.Services.AddDbContext<FilmeContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 // Add services to the container.
 
